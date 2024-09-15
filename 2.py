5e1cf49fa7afdef89ce38e5ed01a06b813cac9ec
s00ndie <s00ndie@icloud.com>
2024-09-15 15:25:15 +0200

    Update and rename 2 to 2.py

--- 2.py
+++ 2.py
@@ -10,3 +10,9 @@ def difference(*nums):
             result = round(result,2)
 
         print(result)
+
+assert difference(1, 2, 3) == 2, 'Test1' 
+assert difference(5, -5) == 10, 'Test2' 
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3' 
+assert difference() == 0, 'Test4' 
+print('OK')
