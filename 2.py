cbb0acba034976b518ad5d0daa49c0fb8c32c838
s00ndie <s00ndie@icloud.com>
2024-09-16 12:32:31 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,7 @@
 def difference(*nums):
     if len(nums) ==0:
         result = 0
-        print(result)
+        return result
     elif len(nums) >1:
         n1 = min(nums)
         n2 = max(nums)
@@ -9,10 +9,10 @@ def difference(*nums):
         if type(result) is float:
             result = round(result,2)
 
-        print(result)
+        return result
 
-assert difference(1, 2, 3) == 2, 'Test1' 
-assert difference(5, -5) == 10, 'Test2' 
-assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3' 
-assert difference() == 0, 'Test4' 
+assert difference(1, 2, 3) == 2, 'Test1'
+assert difference(5, -5) == 10, 'Test2'
+assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
+assert difference() == 0, 'Test4'
 print('OK')
